a7e4975cf9b329840137892aac6d2be9e40cfd46
Multiplataforma15 <luis_garciaa16@hotmail.com>
2015-02-09 10:17:24 -0700

    no message

---
+++ Gato/src/gato/Celdas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gato;
+
+import javax.swing.JButton;
+/**
+ *
+ * @author genesisaurora
+ */
+public class Celdas {
+    
+    JButton A;
+    int B;
+    Celdas()
+    {
+        A=new JButton();
+        B=0;
+    }
+    
+}
